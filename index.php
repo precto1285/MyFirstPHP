94780b9cb407bc6f1935ab5d291659fc590ce711
Philippe Recto <precto1285@gmal.com>
2019-10-09 20:56:29 -0700

    my first PHP code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My First PHP</title>
+</head>
+<body>
+  <?php 
+  $statement= "<h1>Hello World!</h1>";
+  $number=1978;
+  echo $statement;
+  echo "I was born in " . $number;
+  ?>
+</body>
+</html>
